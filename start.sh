f2b872c9329ffb0835050b5bd18d09917a38517c
Artem Vogulev <art.vogulev@gmail.com>
2025-04-25 16:16:20 +0300

    fix github ci

--- start.sh
+++ start.sh
@@ -16,8 +16,8 @@ echo "$4" >> ./.env
 echo "$5" >> ./.env
 echo "$6" >> ./.env
 
-# Ensure, that docker-compose stopped
-docker-compose --env-file ./.env stop
+# Ensure, that docker compose stopped
+docker compose --env-file ./.env stop
 
 # Start new deployment with provided env vars in ./.env file
-docker-compose --env-file ./.env up --build -d
+docker compose --env-file ./.env up --build -d
