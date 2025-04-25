837b0b96dfb175911d973f823e0b34b04847e7ea
Artem Vogulev <art.vogulev@gmail.com>
2025-04-25 16:49:35 +0300

    fix github ci

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure, that docker compose stopped
-'docker compose' --env-file ./.env stop
+docker compose --env-file ./.env stop
 
 # Ensure, that the old application won't be deployed again.
 mvn clean
